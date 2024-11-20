c12c990f23c80d83a14283c424d9f406a41a1b7a
h.habka <h.habka@staff.ososs.com>
2024-11-20 20:30:08 +0400

    test endpoit

--- router.js
+++ router.js
@@ -11,4 +11,18 @@ router.get("/", (req, res) => {
 
 router.get("/redeemPoints/:customerId", redeemPoints)
 
+router.get("/test", (req, res) => {
+  console.log({ req })
+  res.status(200).json({
+    message: "Test get endpoint",
+  })
+})
+
+router.post("/test", (req, res) => {
+  console.log({ req })
+  res.status(200).json({
+    message: "Test post endpoint",
+  })
+})
+
 export default router
